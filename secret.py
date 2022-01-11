21af1b7fe5c1f548c031070a32788712279a31cc
saliouseck2009 <saliouseck2009@gmail.com>
2022-01-11 19:20:57 +0000

    changes

---
+++ secret.py
@@ -0,0 +1,13 @@
+722a3620101c452ab0404279b5066e4c
+this may also be found at: /var/jenkins_home/secrets/initialAdminPassword
+
+
+
+
+echo "# java-exam" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/saliouseck2009/java-exam.git
+git push -u origin main
