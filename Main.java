763ea866c35ba1201aac2652ee5cca21a24e16b7
saliouseck2009 <saliouseck2009@gmail.com>
2022-01-18 00:11:46 +0000

    hook

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("hello, world");
         System.out.println("hello, saliou");
+        System.out.println("hello, hook github");
+        
     }
 }
